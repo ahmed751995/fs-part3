3813ea53eb2cff38528db90cacfeed3ae043b403
Ahmed Abdulrahman <ahmed751995@riseup.net>
2022-12-20 23:38:37 +0200

    Exercises 3.13.-3.14

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,41 +1,17 @@
 const express = require("express");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 const cors = require('cors');
+
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(express.static('build'));
 app.use(cors());
-// morgan.token('data', function(req, resp) {
-//   return JSON.stringify(req.body);
-// });
+app.use(morgan('tiny'));
 
-// const customMorgan = morgan(':method :url :status :res[content-length] - :response-time ms - :data');
-// app.use(customMorgan);
+const Person = require('./models/phonebook');
 
-
-let phonebook = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 
 app.get("/info", (request, response) => {
@@ -44,17 +20,11 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(phonebook);
+  Person.find({}).then(people => response.json(people));
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  let person = phonebook.find((p) => p.id === id);
-  if (!person) {
-    response.status(404).end();
-  } else {
-    response.json(person);
-  }
+  Person.findById(request.params.id).then(person => response.json(person));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -71,22 +41,32 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const person = request.body;
 
-  const find_person = (name) =>
-    phonebook.find((p) => p.name.toLowerCase() === name.toLowerCase());
-
-  if (!person.name || !person.number) {
-    response.status(500).json({ error: "name or number is missing" });
-  } else if (find_person(person.name)) {
-    response.status(500).json({ error: "name already exist" });
-  } else {
-    let new_person = {
-      name: person.name,
-      number: person.number,
-      id: Math.floor(Math.random() * 10000000),
-    };
-    phonebook = phonebook.concat(new_person);
-    response.status(200).json(new_person);
+  if(!person.name) {
+    response.status(400).json({"error": "no name spicified"});
   }
+
+  const newPerson = new Person({
+    name: person.name,
+    number: person.number
+  });
+  newPerson.save().then(savedPerson => response.json(savedPerson));
+
+  // const find_person = (name) =>
+  //   phonebook.find((p) => p.name.toLowerCase() === name.toLowerCase());
+
+  // if (!person.name || !person.number) {
+  //   response.status(500).json({ error: "name or number is missing" });
+  // } else if (find_person(person.name)) {
+  //   response.status(500).json({ error: "name already exist" });
+  // } else {
+  //   let new_person = {
+  //     name: person.name,
+  //     number: person.number,
+  //     id: Math.floor(Math.random() * 10000000),
+  //   };
+  //   phonebook = phonebook.concat(new_person);
+  //   response.status(200).json(new_person);
+  // }
 });
 
 const PORT = process.env.PORT || 8080;
